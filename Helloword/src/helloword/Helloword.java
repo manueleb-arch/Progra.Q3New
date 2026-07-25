0e451be2c07e3e04b8482b2d1066c4aefad3aa41
manue <manue@KiKe>
2026-07-24 21:02:54 -0600

    Aprendi a mostras datos en pantalla y capturar datos

--- Helloword/src/helloword/Helloword.java
+++ Helloword/src/helloword/Helloword.java
@@ -34,7 +34,49 @@ public class Helloword {
         System.out.print("________________________________");
         System.out.println();
         
+        System.out.print("Vamos de camino");
+        System.out.print("la clase de");
+        System.out.print("Progra");
         
+        System.out.println("");
+        
+        
+        System.out.printf("Tu apellido %s es", "Barahona");
+        System.out.println("");
+        
+        System.out.printf("%S Tu apellido es","Barahona");
+        System.out.println("");
+        
+        System.out.printf("Tu apellido%S es", "Barahona");
+        System.out.println("");
+        System.out.println("Hola \n este es \n un nuevo \n amanecer");
+        System.out.print("--------------------------\n");
+        
+        System.out.println("Hola \t este es \t un nuevo \t amanecer");
+       /*
+        \n es para un salto de linea
+        \t es para un tabulado en el parrafo
+        */
+        
+       //Variable
+       int edad; //Tipo entero
+       double peso; //Tipo decimal
+       char inicial; //Tipo Caracter -> letras, digitos, simbolo especiales
+       String nombre; //Tipo cadena
+       boolean estado; //Tipo estado-> true(verdadero) / False(falso)
+       
+       edad = 21; 
+       peso = 60.5;
+       inicial = 'M';
+       nombre = "Manuel";
+       
+        System.out.printf("Tengo %d anios \n ", edad);
+        System.out.printf("Mi peso edeal es %.2f \n", peso);
+        System.out.printf("Dime %c por mi inicial \n", inicial);
+        System.out.printf("%s es mi nombre \n", nombre);
+       
+       
+       
     }//fin de main
     
 }//Fin de class
